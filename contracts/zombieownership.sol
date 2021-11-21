5fc339358966ef9b865de2fc752011f365a6903d
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-20 22:19:30 -0500

    ERC721 Transfer logic implementation

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -12,6 +12,17 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         return zombieToOwner[_tokenId];
     }
 
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) private {
+        ownerZombieCount[_to]++;
+        ownerZombieCount[_from]--;
+        zombieToOwner[_tokenId] = _to;
+        emit Transfer(_from, _to, _tokenId);
+    }
+
     function transferFrom(
         address _from,
         address _to,
