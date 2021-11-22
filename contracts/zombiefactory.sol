bf0b0c3b6801f44d2db30d1ae98ffaa85e1da0f2
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-22 09:52:21 -0500

    Uses the SafeMath lib from OpenZeppelin

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -1,51 +1,49 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./ownable.sol";
+using SafeMath for uint256;
 
 contract ZombieFactory is Ownable {
-    event NewZombie(uint256 zombieId, string name, uint256 dna);
 
-    uint256 dnaDigits = 16;
-    uint256 dnaModulus = 10**dnaDigits;
-    uint256 cooldownTime = 1 days;
+  // 2. Declare using safemath here
 
-    struct Zombie {
-        string name;
-        uint256 dna;
-        uint32 level;
-        uint32 readyTime;
-        uint16 winCount;
-        uint16 lossCount;
-    }
+  event NewZombie(uint zombieId, string name, uint dna);
 
-    Zombie[] public zombies;
+  uint dnaDigits = 16;
+  uint dnaModulus = 10 ** dnaDigits;
+  uint cooldownTime = 1 days;
 
-    mapping(uint256 => address) public zombieToOwner;
-    mapping(address => uint256) ownerZombieCount;
+  struct Zombie {
+    string name;
+    uint dna;
+    uint32 level;
+    uint32 readyTime;
+    uint16 winCount;
+    uint16 lossCount;
+  }
 
-    function _createZombie(string memory _name, uint256 _dna) internal {
-        // 2. Modify new zombie creation here:
-        uint256 id = zombies.push(
-            Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
-        ) - 1;
-        zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
-        emit NewZombie(id, _name, _dna);
-    }
+  Zombie[] public zombies;
 
-    function _generateRandomDna(string memory _str)
-        private
-        view
-        returns (uint256)
-    {
-        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
-        return rand % dnaModulus;
-    }
+  mapping (uint => address) public zombieToOwner;
+  mapping (address => uint) ownerZombieCount;
 
-    function createRandomZombie(string memory _name) public {
-        require(ownerZombieCount[msg.sender] == 0);
-        uint256 randDna = _generateRandomDna(_name);
-        randDna = randDna - (randDna % 100);
-        _createZombie(_name, randDna);
-    }
+  function _createZombie(string memory _name, uint _dna) internal {
+    uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+    zombieToOwner[id] = msg.sender;
+    ownerZombieCount[msg.sender]++;
+    emit NewZombie(id, _name, _dna);
+  }
+
+  function _generateRandomDna(string memory _str) private view returns (uint) {
+    uint rand = uint(keccak256(abi.encodePacked(_str)));
+    return rand % dnaModulus;
+  }
+
+  function createRandomZombie(string memory _name) public {
+    require(ownerZombieCount[msg.sender] == 0);
+    uint randDna = _generateRandomDna(_name);
+    randDna = randDna - randDna % 100;
+    _createZombie(_name, randDna);
+  }
+
 }
