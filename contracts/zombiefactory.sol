ac6ce702a2fb71349d9a167d0d9327f74d1546b6
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-22 10:10:50 -0500

    Using SafeMath 16 and 32

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -1,49 +1,57 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./ownable.sol";
-using SafeMath for uint256;
+import "./safemath.sol";
 
 contract ZombieFactory is Ownable {
+    using SafeMath for uint256;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
 
-  // 2. Declare using safemath here
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
 
-  event NewZombie(uint zombieId, string name, uint dna);
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+    uint256 cooldownTime = 1 days;
 
-  uint dnaDigits = 16;
-  uint dnaModulus = 10 ** dnaDigits;
-  uint cooldownTime = 1 days;
+    struct Zombie {
+        string name;
+        uint256 dna;
+        uint32 level;
+        uint32 readyTime;
+        uint16 winCount;
+        uint16 lossCount;
+    }
 
-  struct Zombie {
-    string name;
-    uint dna;
-    uint32 level;
-    uint32 readyTime;
-    uint16 winCount;
-    uint16 lossCount;
-  }
+    Zombie[] public zombies;
 
-  Zombie[] public zombies;
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
 
-  mapping (uint => address) public zombieToOwner;
-  mapping (address => uint) ownerZombieCount;
+    function _createZombie(string memory _name, uint256 _dna) internal {
+        // Note: We chose not to prevent the year 2038 problem... So don't need
+        // worry about overflows on readyTime. Our app is screwed in 2038 anyway ;)
+        uint256 id = zombies.push(
+            Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
+        ) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+        emit NewZombie(id, _name, _dna);
+    }
 
-  function _createZombie(string memory _name, uint _dna) internal {
-    uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
-    zombieToOwner[id] = msg.sender;
-    ownerZombieCount[msg.sender]++;
-    emit NewZombie(id, _name, _dna);
-  }
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
 
-  function _generateRandomDna(string memory _str) private view returns (uint) {
-    uint rand = uint(keccak256(abi.encodePacked(_str)));
-    return rand % dnaModulus;
-  }
-
-  function createRandomZombie(string memory _name) public {
-    require(ownerZombieCount[msg.sender] == 0);
-    uint randDna = _generateRandomDna(_name);
-    randDna = randDna - randDna % 100;
-    _createZombie(_name, randDna);
-  }
-
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint256 randDna = _generateRandomDna(_name);
+        randDna = randDna - (randDna % 100);
+        _createZombie(_name, randDna);
+    }
 }
