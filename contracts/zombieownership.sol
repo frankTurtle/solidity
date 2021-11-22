11d91da2ca823879a7450cca55733d5582079d90
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-22 10:03:53 -0500

    Replaced the add/sub functions with SafeMath

--- contracts/zombieownership.sol
+++ contracts/zombieownership.sol
@@ -2,8 +2,11 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombieattack.sol";
 import "./erc721.sol";
+import "./safemath.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+    using SafeMath for uint256;
+
     mapping(uint256 => address) zombieApprovals;
 
     function balanceOf(address _owner) external view returns (uint256) {
@@ -19,8 +22,8 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         address _to,
         uint256 _tokenId
     ) private {
-        ownerZombieCount[_to]++;
-        ownerZombieCount[_from]--;
+        ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+        ownerZombieCount[_from] = ownerZombieCount[_from].sub(1);
         zombieToOwner[_tokenId] = _to;
         emit Transfer(_from, _to, _tokenId);
     }
