63c334e6144abf77d17e33b2172814ee9049db97
Big B <8c57e820-f3a7-4561-a664-fed7d85bdedc@4wrd.cc>
2021-11-25 18:33:14 -0500

    updated the functions to use the structs implemented

--- contracts/MappingStructs.sol
+++ contracts/MappingStructs.sol
@@ -23,20 +23,24 @@ contract MappingsStructExample {
     }
 
     function sendMoney() public payable {
-        paymentsMade[msg.sender] += msg.value;
+        paymentsMade[msg.sender].totalBalance += msg.value;
+
+        Payment memory payment = Payment(msg.value, block.timestamp); 
+        paymentsMade[msg.sender].payments[paymentsMade[msg.sender].numPayments] = payment;
+        paymentsMade[msg.sender].numPayments++;
     }
 
     function withdrawMoney(address payable _to, uint _amount) public {
-        uint moneyToSend = paymentsMade[_to]; // get the amount the user has sent in already
+        uint moneyToSend = paymentsMade[_to].totalBalance; // get the amount the user has sent in already
         require(_amount <= moneyToSend, "You can't withdraw more than you have sent"); // verification that the user has that amount
-        paymentsMade[_to] -= moneyToSend;
+        paymentsMade[_to].totalBalance -= moneyToSend;
         _to.transfer(moneyToSend);
     }
 
     function withdrawAllMoney(address payable _to) public {
         // Using the Checks-Effects-Interaction pattern 👇
-        uint moneyToSend = paymentsMade[_to]; // get the amount the user has sent in already
-        paymentsMade[_to] = 0; // wipe it out so they can't withdraw again
+        uint moneyToSend = paymentsMade[_to].totalBalance; // get the amount the user has sent in already
+        paymentsMade[_to].totalBalance = 0; // wipe it out so they can't withdraw again
         _to.transfer(moneyToSend); // send it to them
     }
 }
